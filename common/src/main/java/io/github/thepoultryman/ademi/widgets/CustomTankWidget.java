c172f86a829c2b3854ed1658096d629672de15f8
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2024-05-30 03:13:13 -0400

    Override volume tooltip

--- common/src/main/java/io/github/thepoultryman/ademi/widgets/CustomTankWidget.java
+++ common/src/main/java/io/github/thepoultryman/ademi/widgets/CustomTankWidget.java
@@ -2,7 +2,11 @@ package io.github.thepoultryman.ademi.widgets;
 
 import dev.emi.emi.api.stack.EmiIngredient;
 import dev.emi.emi.api.widget.TankWidget;
+import io.github.thepoultryman.ademi.AdEMI;
 import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
+import net.minecraft.util.FormattedCharSequence;
 
 import java.util.List;
 
@@ -14,6 +18,13 @@ public class CustomTankWidget extends TankWidget {
 
     @Override
     public List<ClientTooltipComponent> getTooltip(int mouseX, int mouseY) {
-        return super.getTooltip(mouseX, mouseY);
+        List<ClientTooltipComponent> tooltip = super.getTooltip(mouseX, mouseY);
+        tooltip.set(
+                tooltip.size() - 2,
+                ClientTooltipComponent.create(FormattedCharSequence.forward(
+                        this.stack.getAmount() + " " + Component.translatable(AdEMI.MOD_ID + ".millibuckets").getString(), Style.EMPTY)
+                )
+        );
+        return tooltip;
     }
 }
